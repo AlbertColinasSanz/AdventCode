e1e50cb271dca66893f0995e0b7edb8fbcd84596
Albert Colinas Sanz <albertcolinas@gmail.com>
2021-12-16 21:02:02 +0100

    cpp init

--- advent_16/main.cpp
+++ advent_16/main.cpp
@@ -1,5 +1,37 @@
+#include <iostream>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fstream>
+#include <string>
+
+
+using namespace std;
+
 int main(int argc, char const *argv[])
 {
-    /* code */
+    std::cout << "argc == " << argc << '\n';
+    
+    std::fstream my_file;
+    std::noskipws;
+
+	my_file.open("data/info.txt", ios::in);
+    
+    if (!my_file) {
+		cout << "File not created!";
+	} else {
+        char ch;
+        string file_info = string();
+        
+		while (my_file >> file_info);
+
+        cout << file_info;
+
+        my_file.close();
+    }
+
+    
+
+    
     return 0;
 }
