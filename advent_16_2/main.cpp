24e32ea55995538e4f88415834d6d38dfcb07ed2
Albert Colinas Sanz <albertcolinas@gmail.com>
2021-12-17 16:54:42 +0100

    pruebas clion

--- advent_16_2/main.cpp
+++ advent_16_2/main.cpp
@@ -7,38 +7,28 @@
 
 using namespace std;
 
-int main(int argc, char const *argv[])
-{
+int main(int argc, char const *argv[]) {
     std::cout << "argc = " << argc << '\n';
 
     std::fstream my_file;
 
     my_file.open("data/info.txt", ios::in);
 
-    if (!my_file)
-    {
+    if (!my_file) {
         cout << "File not created!";
-    }
-    else
-    {
+    } else {
         char ch;
         string file_info = string();
 
-        while (my_file >> std::noskipws >> ch)
-        {
+        while (my_file >> std::noskipws >> ch) {
             // cout << ch;
             file_info += ch;
         };
         // cout << file_info;
 
-        std::copy(std::istreambuf_iterator<char>{my_file},
-                std::istreambuf_iterator<char>{my_file},
-                std::ostreambuf_iterator<char>{file_info});
-
         cout << file_info;
-                
+
         my_file.close();
     }
-
     return 0;
 }
